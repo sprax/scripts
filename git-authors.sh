bbfeb81477dbac28574f095133cd72ba01caa1cd
sprax <sprax@comcast.net>
2019-12-24 14:02:01 -0500

    document git-authors

--- git-authors.sh
+++ git-authors.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 # file: git-authors.sh:
 
-default_code='*.cc *.h *.md *.sh *.txt *.yaml */*.cc */*.h */*.hpp */*.md */*.py */*.sh */*.yml .*/*.yml .circleci/*/*/Dockerfile */*.txt */*/*.cc */*/*.h'
-code_pattern=${1:-$default_code}
+# Count all source lines of code (SLOCS) in files under the current directory
+# that match the (default) code_pattern,
+# attribute each one (including blanks) to an author,
+# group them by author [or the given 'search_field'],
+# and output in descending order:
+#
+#   total-slocs    author
+#
+# Example usage:
+# $ ./git-authors.sh author-mail '*.*sh *.py'
 
-if [[ $# -gt 1 ]] ; then
-  echo "Usage: git-authors.sh code-glob-pattern"
+# single-quoted "source code"-matching pattern:
+default_code='*.cc *.h *.md *.sh *.txt *.yaml */*.cc */*.h */*.hpp */*.md */*.py */*.sh */*.yml .*/*.yml .circleci/*/*/Dockerfile */*.txt */*/*.cc */*/*.h'
+
+# command line arguments with defaults:
+search_field=${1:-$author}          # Example: author-mail
+code_pattern=${2:-$default_code}    # Example: '*.h*'
+
+if [[ $# -gt 2 ]] ; then
+  echo "Usage: git-authors.sh search-field 'code-glob-pattern'"
   exit 0
 fi
 
-git ls-tree -r -z --name-only HEAD -- $code_pattern|xargs -0 -n1 git blame --line-porcelain|grep  "^author "|cut -d' ' -f 2-|sort|uniq -c|sort -nr
+git ls-tree -r -z --name-only HEAD -- $code_pattern \
+    | xargs -0 -n1 git blame --line-porcelain \
+    | grep  "^$search_field " \
+    | cut -d' ' -f 2- \
+    | sort | uniq -c | sort -nr
