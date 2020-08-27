207fe5b0ef15d98dc52f2ac20a8c94a9005a2d22
sprax <sprax@comcast.net>
2020-08-27 00:25:34 -0400

    date

--- git-dated-commit.sh
+++ git-dated-commit.sh
@@ -3,8 +3,8 @@
 
 if [[ $# -ne 2 ]] ; then
   echo 'Usage:   git-dated-commit.sh date message'
-  echo 'Example: git-dated-commit.sh 2020-01-18T12:34:56 "fixed and dated merge"'
+  echo 'Example: git-dated-commit.sh 2020-01-26T12:34:56 "fixed and dated merge"'
   exit 0
 fi
 
-GIT_AUTHOR_DATE=$1 GIT_COMMITTER_DATE=$1 git commit -am "$2"
+GIT_AUTHOR_DATE=$1 GIT_COMMITTER_DATE=$1 git commit -m "$2"
