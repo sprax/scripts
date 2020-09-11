3132fe53e9ce684041ee1c556e14d467ac3ffe15
sprax <sprax@comcast.net>
2020-09-11 00:06:01 -0400

    211

--- git-dated-commit.sh
+++ git-dated-commit.sh
@@ -2,7 +2,7 @@
 # file: git-dated-commit.sh
 message=${1:-'"fixed and dated merge"'}
 
-def_date=2020-02-03T04:05:06
+def_date=2020-02-11T05:06:07
 date_str=${2:-$def_date}
 
 if [[ $# -lt 1 ]] ; then
