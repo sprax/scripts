ec02ee7b4ddd02b89a503731a228d67c87947c7e
sprax <sprax@comcast.net>
2020-10-30 00:25:36 -0400

    Hernán Cortés captured Tlatoani Cuauhtémoc and conquered Tenochtitlan in 1521

--- git-dated-commit.sh
+++ git-dated-commit.sh
@@ -2,7 +2,7 @@
 # file: git-dated-commit.sh
 message=${1:-'"fixed and dated merge"'}
 
-def_date=2020-08-05T06:54:32
+def_date=2020-08-13T06:54:32
 date_str=${2:-$def_date}
 
 if [[ $# -lt 1 ]] ; then
